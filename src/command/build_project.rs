eea74611604445c093709bea452637f6e1f98a37
bvanseg <bvanseg@gmail.com>
2022-10-15 22:37:39 -0400

    Build binary or dynamic library based on project type.

--- src/command/build_project.rs
+++ src/command/build_project.rs
@@ -5,9 +5,7 @@ use crate::{terminal::ansi, config};
 pub fn build_project() {
     let config = config::read_config();
 
-    let c_files = collect_c_file_paths(&config.project_properties.src_dir);
-
-    run_compiler(&config.compiler_properties.compiler_dir, &c_files);
+    run_compiler(&config);
 }
 
 fn collect_c_file_paths(src_dir: &String) -> Vec<String> {
@@ -34,16 +32,31 @@ fn collect_c_file_paths(src_dir: &String) -> Vec<String> {
     return vec;
 }
 
-fn run_compiler(compiler_dir: &String, c_files: &Vec<String>) {
-    let output = std::process::Command::new(compiler_dir)
-        .args(["".to_string()].iter().chain(c_files))
+fn run_compiler(config: &config::Config) {
+    // TODO: Prepend output dir to file names here.
+    // TODO: These file names should be based on the ccake.toml properties.
+    let mut project_args: Vec<String> = match config.project_properties.project_type {
+        config::ProjectType::Binary => vec!["-o", "app.exe"], // TODO: This shouldn't be .exe.
+        // TODO: For library building, there should be a way to distinguish static and dynamic libraries.
+        config::ProjectType::Library => vec!["-shared", "-o", "library.dll"] // TODO: This shouldn't be .dll.
+    }.into_iter().map(String::from).collect();
+    
+    // Collect C source files for inputting into the compiler.
+    let c_files = &mut collect_c_file_paths(&config.project_properties.src_dir);
+    project_args.append(c_files);
+
+    // Run the compiler executable.
+    let output = std::process::Command::new(&config.compiler_properties.compiler_dir)
+        .args(project_args)
         .output()
         .expect("failed to execute compiler process!");
 
+    // If there was standard output from the compiler, emit it.
     let standard_output = &String::from_utf8(output.stdout).expect("Failed to convert output stdout to String!");
-    let ansi_standard_output = ansi::ANSI_ERROR_STYLE.apply(standard_output);
+    let ansi_standard_output = ansi::ANSI_DEFAULT_STYLE.apply(standard_output);
     std::io::stdout().write_all(&ansi_standard_output.as_string().as_bytes()).unwrap();
 
+    // If there was erroroneous output from the compiler, emit it.
     let error_output = &String::from_utf8(output.stderr).expect("Failed to convert output stderr to String!");
     let ansi_err_output = ansi::ANSI_ERROR_STYLE.apply(error_output);
     std::io::stderr().write_all(&ansi_err_output.as_string().as_bytes()).unwrap();
