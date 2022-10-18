0368921993b2d7353f41e1d600bd77fafaad024a
bvanseg <bvanseg@gmail.com>
2022-10-18 06:59:22 -0400

    Added helper functions for warnings using ANSI.

--- src/terminal/ansi.rs
+++ src/terminal/ansi.rs
@@ -1,4 +1,4 @@
-use fansi::{color::AnsiColor, style::AnsiStyle};
+use fansi::{color::AnsiColor, style::AnsiStyle, string::AnsiString};
 
 pub const ANSI_CHOICE_STYLE: [AnsiStyle; 3] = [
     AnsiStyle::Bold,
@@ -7,3 +7,14 @@ pub const ANSI_CHOICE_STYLE: [AnsiStyle; 3] = [
 ];
 pub const ANSI_ERROR_STYLE: [AnsiStyle; 2] =
     [AnsiStyle::Bold, AnsiStyle::ForegroundColor(AnsiColor::Red)];
+pub const ANSI_WARNING_STYLE: [AnsiStyle; 2] =
+    [AnsiStyle::Bold, AnsiStyle::ForegroundColor(AnsiColor::Yellow)];
+
+pub fn warning(text: &str) {
+    println!("{}", warning_text(text));
+}
+
+pub fn warning_text(text: &str) -> String {
+    let warning_label = AnsiString::with_styles_arr("warning:", &ANSI_WARNING_STYLE);
+    return format!("{} {}", warning_label, text);
+}
