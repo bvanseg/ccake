661352de64ba674039b4f51ff31371191e716282
bvanseg <bvanseg@gmail.com>
2022-10-18 06:59:42 -0400

    Made reading .ccake settings a little safer.

--- src/settings.rs
+++ src/settings.rs
@@ -2,11 +2,12 @@ use dirs;
 use serde_derive::{Deserialize, Serialize};
 use std::{fs::File, io::Read};
 
+use crate::terminal::{ansi::{warning}};
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Settings {
     pub default_compiler_dir: String,
 }
-
 static CCAKE_SETTINGS_FILE_NAME: &str = "settings.toml";
 
 pub fn read_settings() -> Settings {
@@ -18,14 +19,37 @@ pub fn read_settings() -> Settings {
 
     home_dir.push(CCAKE_SETTINGS_FILE_NAME);
 
-    let mut file = File::open(home_dir).expect("Failed to open settings.toml file for reading!");
+    let default_settings = Settings {
+        default_compiler_dir: "/path/to/compiler".to_string()
+    };
 
-    let mut file_content = String::new();
-    file.read_to_string(&mut file_content)
-        .expect("Failed to read contents of settings.toml file!");
+    let default_settings_str = toml::to_string(&default_settings).expect("Failed to serialize default .ccake settings!");
 
-    return toml::from_str(&file_content)
-        .expect("Failed to deserialize content from settings.toml file!");
+    // If the .ccake settings file does not exist, create it and write the default data into it.
+    // Otherwise, try to open the file and read into it.
+    if !home_dir.exists() {
+        // TODO: Make this constant somehow.
+        std::fs::write(home_dir, default_settings_str).expect("Failed to write to settings.toml file!");
+        return default_settings;
+    } else {
+        match File::open(home_dir) {
+            Ok(mut file) => {
+                let mut file_content = String::new();
+                let res = file.read_to_string(&mut file_content);
+
+                if let Err(_) = res {
+                    warning("Failed to read text data from settings.toml, falling back on default settings.");
+                    file_content = default_settings_str;
+                }
+
+                return toml::from_str(&file_content).unwrap_or(default_settings);
+            },
+            Err(_) => {
+                warning("Failed to open settings.toml file for reading, falling back on default settings.");
+                return default_settings;
+            }
+        };
+    }
 }
 
 pub fn write_settings(settings: &Settings) {
