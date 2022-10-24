ba0b6ac03b8091b339837894cbde0fddc17ea311
bvanseg <bvanseg@gmail.com>
2022-10-24 05:59:54 -0400

    Formatting.

--- src/command/init.rs
+++ src/command/init.rs
@@ -1,8 +1,7 @@
 use crate::lib;
 
 pub fn new() -> clap::Command {
-    clap::Command::new("init")
-        .about("Creates a new C/C++ project within the current directory.")
+    clap::Command::new("init").about("Creates a new C/C++ project within the current directory.")
 }
 
 pub fn exec() {
