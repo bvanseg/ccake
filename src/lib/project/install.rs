e9bd85f85106479ebcfe10d4de4a3829d8cf70e0
aburd <burdick.aaron@gmail.com>
2022-10-24 19:58:30 +0900

    feat: switch from HashMap to match

--- src/lib/project/install.rs
+++ src/lib/project/install.rs
@@ -1,5 +1,4 @@
 use crate::lib::constants;
-use std::collections::HashMap;
 use std::io::Write;
 
 pub fn install(tool_library_name: Option<&String>) {
@@ -21,7 +20,7 @@ pub fn install(tool_library_name: Option<&String>) {
     #[cfg(unix)]
     let output = std::process::Command::new("sh")
         .arg(dir)
-        .arg(tool_alias(aliases(), tool_library_name))
+        .arg(tool_alias(tool_library_name))
         .output()
         .expect("Failed to install tool/library!");
 
@@ -32,7 +31,7 @@ pub fn install(tool_library_name: Option<&String>) {
         .arg("Bypass")
         .arg("-Command")
         .arg(dir)
-        .arg(tool_alias(aliases(), tool_library_name))
+        .arg(tool_alias(tool_library_name))
         .output()
         .expect("Failed to install tool/library!");
 
@@ -40,43 +39,23 @@ pub fn install(tool_library_name: Option<&String>) {
     std::io::stderr().write_all(&output.stderr).unwrap();
 }
 
-fn tool_alias(aliases: HashMap<String, String>, tool_name: Option<&String>) -> String {
+fn tool_alias(tool_name: Option<&String>) -> String {
     let tool = tool_name.unwrap();
-    let alias = aliases.get(tool);
-    alias.unwrap_or(tool).to_owned()
+    aliases(tool).to_string()
 }
 
 #[cfg(unix)]
-fn aliases() -> HashMap<String, String> {
-    HashMap::new()
+const fn aliases(tool_name: &str) -> &str {
+    match tool_name {
+        _ => tool_name,
+    }
 }
 
 #[cfg(windows)]
-fn aliases() -> HashMap<String, String> {
-    let mut aliases = HashMap::new();
-    aliases.insert("gcc".to_string(), "mingw".to_string());
-    aliases
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn tool_alias_with_alias() {
-        let mut aliases = HashMap::new();
-        aliases.insert("gcc".to_string(), "mingw".to_string());
-        assert_eq!(
-            tool_alias(aliases, Some(&"gcc".to_string())),
-            "mingw".to_string()
-        );
+const fn aliases(tool_name: &str) -> &str {
+    match tool_name {
+        "gcc" => "mingw",
+        _ => tool_name,
     }
-    #[test]
-    fn tool_alias_with_no_alias() {
-        let aliases = HashMap::new();
-        assert_eq!(
-            tool_alias(aliases, Some(&"gcc".to_string())),
-            "gcc".to_string()
-        );
-    }
+    .to_string()
 }
