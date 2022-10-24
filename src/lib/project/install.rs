8b6d859f07f7d856e92509606c9d5a223443e1a4
aburd <burdick.aaron@gmail.com>
2022-10-24 16:34:56 +0900

    feat: allows for aliases on different platforms

--- src/lib/project/install.rs
+++ src/lib/project/install.rs
@@ -1,4 +1,5 @@
 use crate::lib::constants;
+use std::collections::HashMap;
 use std::io::Write;
 
 pub fn install(tool_library_name: Option<&String>) {
@@ -20,7 +21,7 @@ pub fn install(tool_library_name: Option<&String>) {
     #[cfg(unix)]
     let output = std::process::Command::new("sh")
         .arg(dir)
-        .arg(tool_library_name.unwrap())
+        .arg(tool_alias(aliases(), tool_library_name))
         .output()
         .expect("Failed to install tool/library!");
 
@@ -31,10 +32,51 @@ pub fn install(tool_library_name: Option<&String>) {
         .arg("Bypass")
         .arg("-Command")
         .arg(dir)
-        .arg(tool_library_name.unwrap())
+        .arg(tool_alias(aliases(), tool_library_name))
         .output()
         .expect("Failed to install tool/library!");
 
     std::io::stdout().write_all(&output.stdout).unwrap();
     std::io::stderr().write_all(&output.stderr).unwrap();
+}
+
+fn tool_alias(aliases: HashMap<String, String>, tool_name: Option<&String>) -> String {
+    let tool = tool_name.unwrap();
+    let alias = aliases.get(tool);
+    alias.unwrap_or(tool).to_owned()
+}
+
+#[cfg(unix)]
+fn aliases() -> HashMap<String, String> {
+    HashMap::new()
+}
+
+#[cfg(windows)]
+fn aliases() -> HashMap<String, String> {
+    let mut aliases = HashMap::new();
+    aliases.insert("gcc".to_string(), "mingw".to_string());
+    aliases
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tool_alias_with_alias() {
+        let mut aliases = HashMap::new();
+        aliases.insert("gcc".to_string(), "mingw".to_string());
+        assert_eq!(
+            tool_alias(aliases, Some(&"gcc".to_string())),
+            "mingw".to_string()
+        );
+    }
+    #[test]
+    fn tool_alias_with_no_alias() {
+        let aliases = HashMap::new();
+        assert_eq!(
+            tool_alias(aliases, Some(&"gcc".to_string())),
+            "gcc".to_string()
+        );
+    }
 }
